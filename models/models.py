72d40feb2af0082311157d458ce9465bf78c436c
dunmi9791 <dominic.anyanna@gmail.com>
2022-03-17 10:39:51 +0100

    new

--- models/models.py
+++ models/models.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+
+import calendar
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools import float_compare, float_is_zero
 
 
 class AssetImprovementLine(models.Model):
@@ -49,6 +56,46 @@ class AssetImprovementLine(models.Model):
                 lambda m: any(m.asset_improvement_ids.mapped('asset_id.category_id.open_asset'))).post()
         return [x.id for x in created_moves]
 
+    def _prepare_move(self, line):
+        category_id = line.asset_id.category_id
+        account_analytic_id = line.asset_id.account_analytic_id
+        analytic_tag_ids = line.asset_id.analytic_tag_ids
+        depreciation_date = self.env.context.get('depreciation_date') or line.depreciation_date or fields.Date.context_today(self)
+        company_currency = line.asset_id.company_id.currency_id
+        current_currency = line.asset_id.currency_id
+        prec = company_currency.decimal_places
+        amount = current_currency._convert(
+            line.amount, company_currency, line.asset_id.company_id, depreciation_date)
+        asset_name = line.asset_id.name + ' (%s/%s)' % (line.sequence, len(line.asset_id.asset_improvement_ids))
+        move_line_1 = {
+            'name': asset_name,
+            'account_id': category_id.account_depreciation_id.id,
+            'debit': 0.0 if float_compare(amount, 0.0, precision_digits=prec) > 0 else -amount,
+            'credit': amount if float_compare(amount, 0.0, precision_digits=prec) > 0 else 0.0,
+            'partner_id': line.asset_id.partner_id.id,
+            'analytic_account_id': account_analytic_id.id if category_id.type == 'sale' else False,
+            'analytic_tag_ids': [(6, 0, analytic_tag_ids.ids)] if category_id.type == 'sale' else False,
+            'currency_id': company_currency != current_currency and current_currency.id or False,
+            'amount_currency': company_currency != current_currency and - 1.0 * line.amount or 0.0,
+        }
+        move_line_2 = {
+            'name': asset_name,
+            'account_id': category_id.account_asset_id.id,
+            'credit': 0.0 if float_compare(amount, 0.0, precision_digits=prec) > 0 else -amount,
+            'debit': amount if float_compare(amount, 0.0, precision_digits=prec) > 0 else 0.0,
+            'partner_id': line.asset_id.partner_id.id,
+            'analytic_account_id': account_analytic_id.id if category_id.type == 'purchase' else False,
+            'analytic_tag_ids': [(6, 0, analytic_tag_ids.ids)] if category_id.type == 'purchase' else False,
+            'currency_id': company_currency != current_currency and current_currency.id or False,
+            'amount_currency': company_currency != current_currency and line.amount or 0.0,
+        }
+        move_vals = {
+            'ref': line.asset_id.code,
+            'date': depreciation_date or False,
+            'journal_id': category_id.journal_id.id,
+            'line_ids': [(0, 0, move_line_1), (0, 0, move_line_2)],
+        }
+        return move_vals
 
 
 class AccountAsset(models.Model):
